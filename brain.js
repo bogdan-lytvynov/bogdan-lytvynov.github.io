148c3b0ba80bfc8bd6e619b9a2b7dee6d9130236
Bohdan Lytvynov <bogdanl@wix.com>
2019-10-01 09:00:20 +0300

    Mock Wix Maps Iframe

---
+++ brain.js
@@ -0,0 +1,18 @@
+const onMessage = event => {
+  document.getElementById('root').innerHTML = JSON.stringify(event.data)
+}
+
+window.addEventListener('', onMessage)
+
+const selectRoomButton = document.getElementById('room')
+
+const selectRoomPayload = {
+  roomId: '123'
+}
+const sendPostmessageToParent = () => {
+  window.parent.postMessage(selectRoomPayload, '*')
+}
+
+selectRoomButton.addEventListener('click', () => sendPostmessageToParent)
+
+
